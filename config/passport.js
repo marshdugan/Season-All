6e32e53bcbee4c7cfda5f4ea3ee89bef75c138af
Marshall Dugan <marshall.dugan@gmail.com>
2020-03-18 14:31:01 -0400

    working passport and creating trail table

---
+++ config/passport.js
@@ -0,0 +1,80 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+const db  = require('../models');
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user.uuid);
+    });
+
+    passport.deserializeUser(function(uuid, done) {
+        db.Accounts.findByPk(uuid).then(function(user) {
+	        if (user) {
+	            done(null, user.get());
+	        } else {
+	            done(user.errors, null);
+	        }
+	    });
+    });
+
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField : 'account_key',
+        passReqToCallback : true
+    },
+    function(req, email, account_key, done) {
+        process.nextTick(function() {
+        db.Accounts.findOne({
+            where: {email: email}
+        }).then(function(user, err){
+        	if(err) {
+                console.log("err",err)
+                return done(err);
+            } 
+            if (user) {
+            	console.log('signupMessage', 'That email is already taken.');
+                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+            } else {
+                db.Accounts.create({
+                    first_name:req.body.first_name,
+                    last_name:req.body.last_name,
+                    street: req.body.street,
+                    city: req.body.city,
+                    state: req.body.state,
+                    zip: req.body.zip,
+                    balance: req.body.balance,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    account_key: db.Accounts.generateHash(account_key) 
+				}).then(function(dbUser) {		    	
+	                return done(null, dbUser);
+                }).catch(function (err) { console.log(err);}); 
+            }
+        });   
+    });
+}));
+
+passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField : 'account_key',
+    passReqToCallback : true 
+},
+function(req, email, account_key, done) { 
+    db.Accounts.findOne({
+        where: {
+            email: req.body.email 
+        }
+    }).then(function(user, err) {
+        //(!user.validPassword(req.body.account_key)));
+        if (!user){
+            console.log("no user found");
+            return done(null, false, req.flash('loginMessage', 'No user found.')); 
+        }
+        if (user && !user.validPassword(req.body.account_key)){
+            return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); 
+        }
+        return done(null, user);
+    });
+}));
+
+};
